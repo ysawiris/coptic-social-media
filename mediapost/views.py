aada5cf4e07058728d68670e2395c949363334fe
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-12-03 13:27:19 -0800

    [add] proposal

--- mediapost/views.py
+++ mediapost/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponseRedirect
+from django.views.generic import CreateView
+from wiki.forms import PageForm
+from django.urls import reverse_lazy
+from wiki.models import Page
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello Welcome to Coptic Social Media")
+class PostsForFeedListView(CreateView):
