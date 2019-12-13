03a6cbce76a09e5cecc277c1babf875822cdb4e0
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-12-13 12:46:03 -0800

    [add] tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SignUpView_Test(TestCase):
+    """Tests that the pagesworks."""
+    def test_signup_route(self):
+       
+        #Make a GET request, and test route returns 200
+        response = self.client.get('/accounts/signup/')
+    
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_login_route(self):
+       
+        #Make a GET request, and test route returns 200
+        response = self.client.get('/accounts/login/')
+    
+        self.assertEqual(response.status_code, 200)
--- media/tests.py
+++ media/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from media.models import Post
 
-# Create your tests here.
+
+class NewsFeedListView_Test(TestCase):
+    """Tests that the pagea works."""
+    def test_route(self):
+       
+        #Make a GET request, and test route returns 200
+        response = self.client.get('/')
+    
+        self.assertEqual(response.status_code, 200)
+
+class ProfileListView_Test(TestCase):
+    """Tests that the pagea works."""
+    def test_route(self):
+       
+        #Make a GET request, and test route returns 200
+        response = self.client.get('/profile/')
+    
+        self.assertEqual(response.status_code, 200)
+
+class PostCreateNewView_Test(TestCase):
+    """Tests that the pagea works."""
+    def test_route(self):
+       
+        #Make a GET request, and test route returns 200
+        response = self.client.get('/create_new/')
+    
+        self.assertEqual(response.status_code, 200)
